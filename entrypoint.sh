462fb4dd836f6c5ab43848b81f73de2c933110d2
keigo kimpara <sac2ndg+develop@gmail.com>
2021-07-21 22:16:23 +0900

    Update entrypoint.sh
    consider prerelease

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,17 +14,23 @@ else
   cp "$PACKAGE_DIR" package.json
 fi
 
-if [ -z "$FROM_TAG" ]; then
+if [ -z "$FROM_TAG" -a -z "$NOT_PRERELEASE" ]; then
   echo "No from-tag passed. Fallbacking to git previous tag."
-  previous_tag=$(git tag --sort creatordate | tail -n 2 | head -n 1)
+  previous_tag=$(git tag --sort version:refname | grep -e "^v\d*.\d*.\d*$" | tail -n 2 | head -n 1)
+elif [ -z "$FROM_TAG" ]; then
+  echo "No from-tag passed. Fallbacking to git previous tag."
+  previous_tag=$(git tag --sort version:refname | tail -n 2 | head -n 1)
 else
   echo "From-tag detected. Using its value."
   previous_tag=$FROM_TAG
 fi
 
-if [ -z "$TO_TAG" ]; then
+if [ -z "$TO_TAG" -a -z "$NOT_PRERELEASE" ]; then
   echo "No to-tag passed. Fallbacking to git previous tag."
-  new_tag=$(git tag --sort creatordate | tail -n 1)
+  new_tag=$(git tag --sort version:refname | grep -e "^v\d*.\d*.\d*$" | tail -n 1)
+elif [ -z "$TO_TAG" ]; then
+  echo "No to-tag passed. Fallbacking to git previous tag."
+  new_tag=$(git tag --sort version:refname | tail -n 1)
 else
   echo "To-tag detected. Using its value."
   new_tag=$TO_TAG
